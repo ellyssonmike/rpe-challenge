63ba7ea31dd72f4252157f5ea6a31bb95eced0be
Elysson Marconi <ellyssonmiike@hotmail.com>
2026-05-26 20:39:06 -0300

    feat(processor/docs): added documentation decorators on PaymentProcess dtos

--- payment-processor/src/payment/api/dtos/payment-process.request.ts
+++ payment-processor/src/payment/api/dtos/payment-process.request.ts
@@ -1,24 +1,43 @@
-import { IsCPF } from '@infra/common/decorators/validation/is-cpf.decorator';
 import { IsEnum, IsNotEmpty, IsNumber, IsUUID, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { PaymentMethod } from 'src/payment/domain/enums/payment-method.enum';
+import { IsCPF } from '@infra/common/decorators/validation/is-cpf.decorator';
 
 export class PaymentProcessRequest {
+  @ApiProperty({ enum: PaymentMethod, description: 'Método de pagamento' })
   @IsEnum(PaymentMethod)
   paymentMethod: PaymentMethod;
 
+  @ApiProperty({ type: String, format: 'uuid', description: 'ID da ordem' })
   @IsUUID()
   orderId: string;
 
+  @ApiProperty({
+    type: String,
+    format: 'uuid',
+    description: 'ID do item a ser efetuado o pagamento',
+  })
   @IsUUID()
   itemId: string;
 
+  @ApiProperty({
+    type: Number,
+    format: 'int64',
+    description: 'Valor total do pagamento (em centavos)',
+  })
   @IsNumber()
   @Min(1)
   amount: number;
 
+  @ApiProperty({ type: String, description: 'Nome do comprador' })
   @IsNotEmpty()
   buyerName: string;
 
+  @ApiProperty({
+    type: String,
+    format: 'formato: "12345678910" ou "123.456.789-10"',
+    description: 'CPF do comprador',
+  })
   @IsNotEmpty()
   @IsCPF()
   buyerCpf: string;
--- payment-processor/src/payment/api/dtos/payment-process.response.ts
+++ payment-processor/src/payment/api/dtos/payment-process.response.ts
@@ -1,8 +1,24 @@
 import { Nullable } from 'src/shared/types/nullable';
 import { PaymentStatus } from '../../domain/enums/payment-status.enum';
+import { ApiOkResponse, ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { SwaggerResponse } from '@docs/index';
 
-export abstract class PaymentProcessResponse {
+@SwaggerResponse(ApiOkResponse)
+export class PaymentProcessResponse {
+  @ApiProperty({ enum: PaymentStatus, description: 'Status do pagamento' })
   paymentStatus: PaymentStatus;
+
+  @ApiPropertyOptional({
+    type: Date,
+    format: 'date-time',
+    description: 'Data do pagamento efetivado (quando status for PAID)',
+  })
   paymentDate: Nullable<Date>;
+
+  @ApiProperty({
+    type: Date,
+    format: 'date-time',
+    description: 'Data de processamento do pagamento',
+  })
   processedAt: Date;
 }
