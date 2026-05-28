b9a3ecf423421c691db5a548b386928868693bd8
Elysson Marconi <ellyssonmiike@hotmail.com>
2026-05-27 23:05:54 -0300

    fix(orders/web): added missing slash

--- orders-web/src/services/api/client/client.service.ts
+++ orders-web/src/services/api/client/client.service.ts
@@ -4,7 +4,7 @@ class ClientHTTPService extends HTTPService {
   public async request<T>(method: RequestMethod, endpoint: string, body?: T, headers?: HeadersInit) {
     const response = await super.request(method, endpoint, body, headers);
     if (response.status === 401) {
-      window.location.href = 'auth/logout';
+      window.location.href = '/auth/logout';
     }
 
     return response;
